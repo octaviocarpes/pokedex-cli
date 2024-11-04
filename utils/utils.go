94fe5a76ed4d711a25a01d25f786fee74fa38c4d
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-04 20:14:40 -0300

    chore: add util functions to convert data to byte slice

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -25,4 +26,25 @@ func ClearConsole() {
 	for i := 0; i < 50; i++ {
 		fmt.Printf("\n")
 	}
+}
+
+func ConvertToByteSlice(payload any) []byte {
+	result, marshalError := json.Marshal(payload)
+
+	if marshalError != nil {
+		return nil
+	}
+
+	return result
+}
+
+func ConvertFromByteSlice[T any](payload []byte) (T, error) {
+	var result T
+	unmarshalError := json.Unmarshal(payload, &result)
+
+	if unmarshalError != nil {
+		return result, unmarshalError
+	}
+
+	return result, nil
 }
