a035cae3970f4549d46ef8b2c7e6a9056f19ce98
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-04 20:39:08 -0300

    chore: add cache clean every 10s

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/octaviocarpes/pokedex-cli/commands"
+	"github.com/octaviocarpes/pokedex-cli/poke-api/cache"
 	"github.com/octaviocarpes/pokedex-cli/utils"
 )
 
 func main() {
+	cache.NewCache(10 * time.Second)
 	cliCommands := commands.ListAllCommands()
 	var userInput string
 	userInput = utils.RequestUserInput()
