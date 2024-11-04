a925f4f61279c52e8203fbba8191f8b3fed3699f
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-04 20:14:07 -0300

    chore: add caching to  poke_api package

--- poke-api/poke_api.go
+++ poke-api/poke_api.go
@@ -1,10 +1,12 @@
 package poke_api
 
 import (
+	"log"
 	"strconv"
 
 	http_client "github.com/octaviocarpes/pokedex-cli/http-client"
 	cache "github.com/octaviocarpes/pokedex-cli/poke-api/cache"
+	utils "github.com/octaviocarpes/pokedex-cli/utils"
 )
 
 type ListLocationsResponse struct {
@@ -17,18 +19,49 @@ type ListLocationsResponse struct {
 	} `json:"results"`
 }
 
+func cacheLocation(url string, location ListLocationsResponse) {
+	byteSlice := utils.ConvertToByteSlice(location)
+
+	if byteSlice != nil {
+		cache.Add(url, byteSlice)
+	}
+}
+
+func getCachedLocation(url string) (ListLocationsResponse, error) {
+	cachedResponse, ok := cache.Get(url)
+	response := &ListLocationsResponse{}
+
+	if ok {
+		parsedResponse, conversionError := utils.ConvertFromByteSlice[ListLocationsResponse](cachedResponse)
+
+		if conversionError != nil {
+			log.Fatal("Failed to decode data from byte slice")
+		}
+
+		return parsedResponse, nil
+	}
+
+	return *response, nil
+}
+
 func GetLocations(offset int) (ListLocationsResponse, error) {
 	stringValue := strconv.Itoa(offset)
 	search := "offset=" + stringValue
 	url := "/location?" + search
 
-	cachedResponse := cache.Get(url)
+	cachedLocation, getCachedError := getCachedLocation(url)
 
-	if res, ok := cachedResponse.(ListLocationsResponse); ok {
-		return res, nil
+	if getCachedError != nil {
+		log.Fatal(getCachedError)
+	}
+
+	if cachedLocation.Results != nil {
+		return cachedLocation, nil
 	}
 
 	response, error := http_client.ExecuteGet[ListLocationsResponse](url)
+
+	cacheLocation(url, response)
 
 	if error != nil {
 		return response, error
