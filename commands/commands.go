a359f189c536330b5ff3bca2147f8309aee67185
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-06 19:00:20 -0300

    chore: update commands.go

--- commands/commands.go
+++ commands/commands.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	explore_command "github.com/octaviocarpes/pokedex-cli/commands/explore"
 	map_command "github.com/octaviocarpes/pokedex-cli/commands/map"
 )
 
 type cliCommand struct {
 	name     string
-	Callback func() error
+	Callback func(args []string) error
 }
 
-func HelpCallback() error {
+func HelpCallback(args []string) error {
 	fmt.Println(`
 Here is a list of all available cli Commands:
 
@@ -23,23 +24,33 @@ Here is a list of all available cli Commands:
 	- map: Fetch the locations of the Pokemon World (Onwards)
 	
 	- mapb: Fetch the locations of the Pokemon World (Backwards)
+
+	- explore: Explore a location of the Pokemon world and shows a list of Pokemons you can encounter.
+		To use the explore command you need to pass a location as a parameter eg:
+			   
+		Pokedex CLI > explore mt-coronet-2f
 	`)
 
 	return nil
 }
 
-func exitCallback() error {
+func exitCallback(args []string) error {
 	os.Exit(0)
 	return nil
 }
 
-func mapCallback() error {
+func mapCallback(args []string) error {
 	err := map_command.GetLocations()
 	return err
 }
 
-func mapbCallback() error {
+func mapbCallback(args []string) error {
 	err := map_command.GetBackwardLocations()
+	return err
+}
+
+func exploreCallback(args []string) error {
+	err := explore_command.ListEncounters()
 	return err
 }
 
@@ -60,6 +71,10 @@ func ListAllCommands() map[string]cliCommand {
 		"mapb": {
 			name:     "mapb",
 			Callback: mapbCallback,
+		},
+		"explore": {
+			name:     "explore",
+			Callback: exploreCallback,
 		},
 	}
 
