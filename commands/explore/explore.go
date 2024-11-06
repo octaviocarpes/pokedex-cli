5149a6dd064ca42b778537083ec8241aa7d92784
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-06 18:59:57 -0300

    chore: create explore pkg

---
+++ commands/explore/explore.go
@@ -0,0 +1,8 @@
+package explore_command
+
+import "fmt"
+
+func ListEncounters() error {
+	fmt.Print("list encounters\n")
+	return nil
+}
---
+++ commands/explore/go.mod
@@ -0,0 +1,3 @@
+module github.com/octaviocarpes/pokedex-cli/commands/explore
+
+go 1.23.2
