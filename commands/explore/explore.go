9a2eb42a9ecd5fad9323b1404bd6fd6af6cf7b46
Octavio Carpes <octavio.carpes10@gmail.com>
2024-11-07 05:34:38 -0300

    chore: add list encounter feature

--- commands/explore/explore.go
+++ commands/explore/explore.go
@@ -1,8 +1,23 @@
 package explore_command
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func ListEncounters() error {
-	fmt.Print("list encounters\n")
+	api "github.com/octaviocarpes/pokedex-cli/poke-api"
+)
+
+func ListEncounters(location string) error {
+
+	response, err := api.GetLocationArea(location)
+
+	if err != nil {
+		log.Fatal("failed to explore")
+	}
+
+	for _, encounter := range response.PokemonEncounters {
+		fmt.Printf("%v\n", encounter.Pokemon.Name)
+	}
+
 	return nil
 }
